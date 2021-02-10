7fec18da81f91d705e7186a04d47e1875cf95c0a
Alexwprof <47756829+Alexwprof@users.noreply.github.com>
2021-02-10 22:15:06 +0300

    Update htaccess.php

--- SEO/htaccess.php
+++ SEO/htaccess.php
@@ -5,3 +5,13 @@ RewriteCond %{THE_REQUEST} //
 # Проверяем, повторяется ли слеш (//) более двух раз. 
 RewriteRule .* /$0 [R=301,L] 
 # Исключаем все лишние слеши.
+
+# с http на https
+RewriteEngine On 
+RewriteCond %{ENV:HTTPS} !on 
+RewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
+
+# с www на без  www
+RewriteEngine On
+RewriteCond %{HTTP_HOST} ^www.solncevo.dr-vita.ru$ [NC]
+RewriteRule ^(.*)$ http://solncevo.dr-vita.ru/$1 [R=301,L]
