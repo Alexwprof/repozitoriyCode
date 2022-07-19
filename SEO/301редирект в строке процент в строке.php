4f601984ffe23f847dc762d6643eb9b0fde717b2
Alexwprof <47756829+Alexwprof@users.noreply.github.com>
2022-07-19 22:16:46 +0300

    Update 301редирект в строке процент в строке.php

--- SEO/301редирект в строке процент в строке.php
+++ SEO/301редирект в строке процент в строке.php
@@ -1,1 +1,14 @@
 
+<?
+     /** /gjjhfh%    **/
+    $re2 = '/^\/(.+%)/m';
+    preg_match_all($re2, $_SERVER["REQUEST_URI"], $matches2, PREG_SET_ORDER, 0);
+    if(!empty($matches2)){header('Location: /',true, 301);}
+    
+    /** /arenda/gjjhfh%    **/
+    $re = '/^\/(arenda)\/(.+%)/m';
+    preg_match_all($re, $_SERVER["REQUEST_URI"], $matches, PREG_SET_ORDER, 0);
+    if(!empty($matches)){header('Location: /arenda/',true, 301);}
+
+
+?>
